68eb20dee3ad978962d2cd222c3e00d5041b2777
Raimundo Neto <rasneto147@gmail.com>
2022-09-26 12:26:14 -0300

    topbars and pages create

---
+++ src/pages/Calls.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Calls = () => {
+  return(
+    <View>
+      <Text>Calls</Text>
+    </View>
+  );
+}
+
+export default Calls;
---
+++ src/pages/Status.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Status = () => {
+  return(
+    <View>
+      <Text>Status</Text>
+    </View>
+  );
+}
+
+export default Status;
